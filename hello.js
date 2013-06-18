eb0f48acfd9832959c8fde8008f46c6c209adf05
cokeefe <chaemobile@gmail.com>
2013-06-18 13:47:34 -0400

    update files

--- hello.js
+++
@@ -1,6 +0,0 @@
-function hello(param){
-	if (arguments.length == 1){
-		return "Hello, " + param + "!";
-	} else
-		return "Hello!"; 
-}
--- properties.js
+++
@@ -1,7 +0,0 @@
-function setSomePropertiesOn(name){
-	name.x = 7;
-	name.y = 8;
-	name.onePlus = function(number){
-		number + 1;
-	};
-}
