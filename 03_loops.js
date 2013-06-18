f319749aa9148acbf2c069ce13dcf5b69a19a084
cokeefe <chaemobile@gmail.com>
2013-06-18 14:17:41 -0400

    update loops array

--- 03_loops.js
+++ 03_loops.js
@@ -24,3 +24,15 @@ function join(array_list, symbol){
 
 	return result
 };
+
+function sum(numbers){
+	var result = 0
+	for (var i = 0; i < numbers.length; i++){
+		result += numbers[i];
+	};
+	return result
+};
+
+function paramify(){
+
+};
