ffd267f4315a06e5161247ce89615959bfccb99c
cokeefe <chaemobile@gmail.com>
2013-06-18 14:12:35 -0400

    update loops

--- 03_loops.js
+++ 03_loops.js
@@ -5,3 +5,22 @@ function repeat(msg, repeats) {
 	};
 	return result;
 };
+
+function join(array_list, symbol){
+	var delimeter = symbol || ""
+	var result = ""
+
+	if (delimeter == symbol){
+		for (var i = 0; i < array_list.length-1; i++){
+			result += array_list[i] + delimeter;
+		};
+		result += array_list[array_list.length-1]
+
+	} else {
+		for (var i = 0; i <= array_list.length-1; i++){
+			result += array_list[i];
+		};
+	};
+
+	return result
+};
